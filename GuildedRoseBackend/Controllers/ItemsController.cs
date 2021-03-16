aa62432646f5e6da35b316af956b80ff2a3c1d75
Greg Orton <ortongreg@gmail.com>
2021-03-16 15:26:42 -0500

    added url parameter example

--- GuildedRoseBackend/Controllers/ItemsController.cs
+++ GuildedRoseBackend/Controllers/ItemsController.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace GuildedRoseBackend.Controllers
 {
@@ -15,13 +14,22 @@ namespace GuildedRoseBackend.Controllers
 
         private readonly ILogger<ItemsController> _logger;
 
+        private Item[] _allItems = new Item[] {
+                new Item { id = 1, name = "Foo", quality = 10, sellIn = 11 },
+                new Item { id = 2, name = "Bar", quality = 12, sellIn = 13 },
+            };
+
         public ItemsController(ILogger<ItemsController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<Item> Get() => 
-            new Item[] { new Item { id = 1, name = "Foo", quality = 10, sellIn = 11 } };
+        public IEnumerable<Item> Get() => _allItems;
+
+
+        // GET items/1
+        [HttpGet("{id}")]
+        public Item GetQuery(string id) => _allItems.First(item => item.id == Int32.Parse(id));
     }
 }
