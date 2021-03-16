00e2af2f40e39212199da3cc9f71a67415e47f6c
Greg Orton <ortongreg@gmail.com>
2021-03-16 15:18:57 -0500

    initial commit with items endpoint

---
+++ GuildedRoseBackend/Controllers/ItemsController.cs
@@ -0,0 +1,27 @@
+﻿using GuildedRoseBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuildedRoseBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+
+        private readonly ILogger<ItemsController> _logger;
+
+        public ItemsController(ILogger<ItemsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> Get() => 
+            new Item[] { new Item { id = 1, name = "Foo", quality = 10, sellIn = 11 } };
+    }
+}
---
+++ GuildedRoseBackend/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace GuildedRoseBackend.Models
+{
+    public class Item
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int quality { get; set; }
+        public int sellIn { get; set; }
+    }
+}
