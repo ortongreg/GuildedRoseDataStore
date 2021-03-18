e4a7135c495be508d192e1f1e58956b3569f56d4
Greg Orton <ortongreg@gmail.com>
2021-03-18 13:52:12 -0500

    internal backstage pass path

--- GuildedRoseItemUpdatesAPI/GuildedRose.cs
+++ GuildedRoseItemUpdatesAPI/GuildedRose.cs
@@ -27,44 +27,18 @@ namespace GuildedRoseItemUpdatesAPI
             if(item.Name == "Aged Brie")
             {
                 UpdateAgedBrie(item);
+            }else if(item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                UpdateBackstagePass(item);
             }
+
             else
             {
-
-                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Quality > 0)
                 {
-                    if (item.Quality > 0)
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-                        }
+                        item.Quality = item.Quality - 1;
                     }
                 }
 
@@ -75,23 +49,49 @@ namespace GuildedRoseItemUpdatesAPI
 
                 if (item.SellIn < 0)
                 {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Quality > 0)
                     {
-                        if (item.Quality > 0)
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
+                            item.Quality = item.Quality - 1;
                         }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
                     }
                 }
 
             }
+        }
+
+        private static void UpdateBackstagePass(Item item)
+        {
+                if (item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
+
+                    if (item.SellIn < 11)
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+                        }
+                    }
+
+                    if (item.SellIn < 6)
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+                        }
+                    }
+                        
+                }
+                
+
+                item.SellIn = item.SellIn - 1;
+
+                if (item.SellIn < 0)
+                {
+                    item.Quality = item.Quality - item.Quality;   
+                }
         }
 
         private static void UpdateAgedBrie(Item item)
