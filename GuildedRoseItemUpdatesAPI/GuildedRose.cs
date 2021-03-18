282d9032ad81c0627a5693e95e03a3e8a419a152
Greg Orton <ortongreg@gmail.com>
2021-03-18 13:54:30 -0500

    internal sulfruas path

--- GuildedRoseItemUpdatesAPI/GuildedRose.cs
+++ GuildedRoseItemUpdatesAPI/GuildedRose.cs
@@ -30,35 +30,33 @@ namespace GuildedRoseItemUpdatesAPI
             }else if(item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
                 UpdateBackstagePass(item);
+            }else if(item.Name == "Sulfuras, Hand of Ragnaros")
+            {
+                UpdateSulfuras(item);
             }
 
             else
             {
                 if (item.Quality > 0)
                 {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
+                    item.Quality = item.Quality - 1;
                 }
 
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.SellIn = item.SellIn - 1;
-                }
+                item.SellIn = item.SellIn - 1;
 
                 if (item.SellIn < 0)
                 {
                     if (item.Quality > 0)
                     {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
+                        item.Quality = item.Quality - 1;
                     }
                 }
 
             }
+        }
+
+        private static void UpdateSulfuras(Item item)
+        {
         }
 
         private static void UpdateBackstagePass(Item item)
